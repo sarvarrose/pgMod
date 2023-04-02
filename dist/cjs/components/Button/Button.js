ef5a834426ac7003f43552aee1980dc77092d6ef
Sarvar Kumar <sarvarrose@gmail.com>
2023-04-02 18:13:42 +0530

    create react component library

---
+++ dist/cjs/components/Button/Button.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tslib_1 = require("tslib");
+var react_1 = tslib_1.__importDefault(require("react"));
+var Button = function (props) {
+    return react_1.default.createElement("button", null, props.label);
+};
+exports.default = Button;
+//# sourceMappingURL=Button.js.map
---
+++ dist/esm/components/Button/Button.js
@@ -0,0 +1,6 @@
+import React from "react";
+var Button = function (props) {
+    return React.createElement("button", null, props.label);
+};
+export default Button;
+//# sourceMappingURL=Button.js.map
